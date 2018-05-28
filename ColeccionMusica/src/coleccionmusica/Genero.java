339855f9bc4097aa4c3b1b070e66882bd7781aee
JESSICA AMOGUIMBA <38404267+JESSICAAMOGUIMBA@users.noreply.github.com>
2018-05-28 07:24:31 -0500

    Add files via upload

---
+++ ColeccionMusica/src/coleccionmusica/Genero.java
@@ -0,0 +1,29 @@
+
+package coleccionmusica;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Genero {
+    public String genero;
+    
+    //CONSTRUCTOR
+
+    public Genero() {
+        
+    }
+    
+   // GET Y SET
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
+    
+}
